738055fa0177a1157dc1d26e999b0a0360b55030
William Ahrons <william.ahrons@gmail.com>
2017-03-25 18:39:11 -0300

    Autowire list field

--- src/main/java/mqt/spring/example/controller/SimplePage.java
+++ src/main/java/mqt/spring/example/controller/SimplePage.java
@@ -4,6 +4,8 @@ package mqt.spring.example.controller;
  * Created by wahrons on 25/03/17.
  */
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,6 +15,8 @@ import java.util.List;
 @Controller
 public class SimplePage {
 
+    @Autowired
+    @Qualifier("cache")
     private List<String> messages;
 
 
