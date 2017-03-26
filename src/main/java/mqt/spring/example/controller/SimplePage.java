0f86eab02384ba47826f7664552b4c61b29292ea
William Ahrons <william.ahrons@gmail.com>
2017-03-25 22:40:51 -0300

    Fix SimplePage

--- src/main/java/mqt/spring/example/controller/SimplePage.java
+++ src/main/java/mqt/spring/example/controller/SimplePage.java
@@ -5,7 +5,6 @@ package mqt.spring.example.controller;
  */
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,7 +15,6 @@ import java.util.List;
 public class SimplePage {
 
     @Autowired
-    @Qualifier("cache")
     private List<String> messages;
 
 
